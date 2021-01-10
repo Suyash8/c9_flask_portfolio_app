9bdfb1a8f0127a91bb62a31550c1cc2189880814
Suyash8 <sharmasuyash821+cc@gmail.com>
2021-01-10 08:15:10 +0530

    Syntax fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 @app.route('/<name>')
 def profile(name):
   name1 = ", " + str(name)
-	return render_t0emplate('index.html', name=name1)
+	return render_template('index.html', name=name1)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
