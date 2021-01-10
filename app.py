0605857ae060438cfc5a661d9f2b791fc2013a93
Suyash8 <sharmasuyash821+cc@gmail.com>
2021-01-10 21:04:25 +0530

    Test commit name

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name1)
+	return render_template('index.html', name=", {}".format(name))
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
